f0de4e7c4a8a1571414e2f93123e64d7f9a4e54a
simon9527 <freefanchao@gmail.com>
2015-12-29 13:00:31 +0800

    prepare
    change a litte in the initialization,just creat a simple "main activity"

--- app/src/main/java/com/simonmeng/demo/activity/SplashActivity.java
+++ app/src/main/java/com/simonmeng/demo/activity/SplashActivity.java
@@ -1,7 +1,9 @@
-package com.simonmeng.demo;
+package com.simonmeng.demo.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.simonmeng.demo.R;
 
 public class SplashActivity extends AppCompatActivity {
 
