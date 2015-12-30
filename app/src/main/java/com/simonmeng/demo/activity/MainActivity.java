135aace690adef7d68cece1aec6d6cc4743b9c3b
simon9527 <freefanchao@gmail.com>
2015-12-30 17:48:07 +0800

    activity_splash complete

--- app/src/main/java/com/simonmeng/demo/activity/MainActivity.java
+++ app/src/main/java/com/simonmeng/demo/activity/MainActivity.java
@@ -2,8 +2,14 @@ package com.simonmeng.demo.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.simonmeng.demo.R;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void connectNet(View view){
+        new Thread(){
+            public void run(){
+                super.run();
+                try {
+                    URL url = new URL("http://gdown.baidu.com/data/wisegame/2fa30ed4c1f993cc/momo_573.apk");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+                    conn.setConnectTimeout(5000);
+                    int code = conn.getResponseCode();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 }
--- app/src/main/java/com/simonmeng/demo/activity/SplashActivity.java
+++ app/src/main/java/com/simonmeng/demo/activity/SplashActivity.java
@@ -1,15 +1,36 @@
 package com.simonmeng.demo.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.simonmeng.demo.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private Button bt_splash_start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        bt_splash_start = (Button) findViewById(R.id.bt_splash_start);
+        //设置字体，typeface字体。
+        Typeface face = Typeface.createFromAsset(getAssets(),"Walkway Bold.ttf");
+        bt_splash_start.setTypeface(face);
+        bt_splash_start.setTransformationMethod(null);
+
+
+
+    }
+
+    public void toMainActivity(View view){
+        Intent openMainActivityIntent = new Intent();
+        openMainActivityIntent.setClass(SplashActivity.this,MainActivity.class);
+        startActivity(openMainActivityIntent);
+        finish();
     }
 }
