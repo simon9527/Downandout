b451637fb2897c3071ee5a4c525ad150e18a3425
simon9527 <freefanchao@gmail.com>
2016-01-04 21:19:13 +0800

    初步完成天气搜索，完成了返回json的解析。

--- app/src/main/java/com/simonmeng/demo/activity/WeatherActivity.java
+++ app/src/main/java/com/simonmeng/demo/activity/WeatherActivity.java
@@ -9,10 +9,9 @@ import android.widget.TextView;
 import com.baidu.apistore.sdk.ApiCallBack;
 import com.baidu.apistore.sdk.ApiStoreSDK;
 import com.baidu.apistore.sdk.network.Parameters;
+import com.google.gson.Gson;
 import com.simonmeng.demo.R;
-
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.simonmeng.demo.domain.WeatherBean;
 
 public class WeatherActivity extends AppCompatActivity {
     TextView tv_weather_location;
@@ -35,13 +34,17 @@ public class WeatherActivity extends AppCompatActivity {
         tv_weather_maxmin.setTypeface(face);
         tv_weather_temperature.setTypeface(face);
         //bt_splash_start.setTransformationMethod(null);
+
         quaryWeather();
+
 
     }
 
-    public void quaryWeather(){
+    ;
+
+    public void quaryWeather() {
         Parameters para = new Parameters();
-        para.put("city", "沈阳");
+        para.put("city", "北京");
         ApiStoreSDK.execute("http://apis.baidu.com/heweather/weather/free",
                 ApiStoreSDK.GET,
                 para,
@@ -50,14 +53,25 @@ public class WeatherActivity extends AppCompatActivity {
                     public void onSuccess(int status, String responseString) {
                         Log.i("sdkdemo", "onSuccess");
                         //Gson解析服务器返回的json数据---responseString
+                        String cityWeather = responseString.replace("HeWeather data service 3.0","CityWeather");
+                        System.out.print(cityWeather);
+                        Gson gson = new Gson();
+                        WeatherBean bean = gson.fromJson(cityWeather, WeatherBean.class);
+                        String weather_location = bean.CityWeather.get(0).basic.city;
+                        int weather_pm = bean.CityWeather.get(0).aqi.city.pm25;
+                        String weather_qlty = bean.CityWeather.get(0).aqi.city.qlty;
+                        int max = bean.CityWeather.get(0).daily_forecast.get(0).tmp.max;
+                        int min = bean.CityWeather.get(0).daily_forecast.get(0).tmp.min;
+                        int tmp = bean.CityWeather.get(0).now.tmp;
+                        tv_weather_location.setText(weather_location);
+                        tv_weather_pm.setText("Pm2.5:"+weather_pm+"\n"+weather_qlty);
+                        tv_weather_maxmin.setText("Max ↑"+max+"°\n  Mix ↓"+min+"°");
+                        tv_weather_temperature.setText(tmp+"°C");
 
-                       // JSONObject jsonObject = new JSONObject(responseString);
-                        try {
-                            JSONObject jsonObject = new JSONObject(responseString);
-                            tv_weather_location.setText("123");
-                        }catch (JSONException jsonException){
-                            jsonException.printStackTrace();
-                        }
+
+
+
+
                     }
 
                     @Override
@@ -74,4 +88,5 @@ public class WeatherActivity extends AppCompatActivity {
 
                 });
     }
+
 }
---
+++ app/src/main/java/com/simonmeng/demo/domain/WeatherBean.java
@@ -0,0 +1,152 @@
+package com.simonmeng.demo.domain;
+
+import java.util.List;
+
+public class WeatherBean {
+    //0阶---------
+    public List<WeatherDetail> CityWeather;
+
+    public class WeatherDetail{
+        public Aqi aqi;
+        public Basic basic;
+        public List<Daily_forecast> daily_forecast;
+        public List<Hourly_forecast> hourly_forecast;
+        public Now now;
+        public String status;
+        public Suggestion suggestion;
+    }
+
+
+
+    //1阶---------
+    public class Aqi{
+        public City city;
+    }
+    public class Basic{
+        //这里六位小数点可能是double或者float
+        public String city;
+        public String cnty;
+        public String id;
+        public double lat;
+        public double lon;
+        public Update update;
+    }
+    public class Daily_forecast{
+        public Astro astro;
+        public Cond cond;
+        public String date;
+        public int hum;
+        public double pcpn;
+        public double pop;
+        public int pres;
+        public Tmp tmp;
+        public int vis;
+        public Wind wind;
+    }
+    public class Hourly_forecast{
+        public String date;
+        public int hum;
+        public int pop;
+        public int pres;
+        public int tmp;
+        public Wind wind;
+    }
+    public class Now{
+        public Cond cond;
+        public int fl;
+        public int hum;
+        public int pcpn;
+        public int pres;
+        public int tmp;
+        public int vis;
+        public Wind wind;
+    }
+    public class Suggestion{
+        public Comf comf;
+        public Cw cw;
+        public Drsg drsg;
+        public Flu flu;
+        public Sport sport;
+        public Trav trav;
+        public Uv uv;
+    }
+
+    //2阶---------
+    //属于aqi
+    public class City{
+        public int aqi;
+        public int co;
+        public int no2;
+        public int o3;
+        public int pm10;
+        public int pm25;
+        public String qlty;
+        public int so2;
+    }
+
+    //属于basic
+    public class Update{
+        public String loc;
+        public String utc;
+    }
+
+    //属于daily_forecast
+    public class Astro{
+        public String sr;
+        public String ss;
+    }
+    public class Cond{
+        public int code_d;
+        public int code_n;
+        public String txt_d;
+        public String txt_n;
+        public int code;
+        public String txt;
+    }
+    public class Tmp{
+        public int max;
+        public int min;
+    }
+    public class Wind{
+        public int deg;
+        public String dir;
+        public String sc;
+        public int spd;
+    }
+
+
+    //属于now
+
+    //属于suggestion
+    public class Comf{
+        public String brf;
+        public String txt;
+    }
+    public class Cw{
+        public String brf;
+        public String txt;
+    }
+    public class Drsg{
+        public String brf;
+        public String txt;
+    }
+    public class Flu{
+        public String brf;
+        public String txt;
+    }
+    public class Sport{
+        public String brf;
+        public String txt;
+    }
+    public class Trav{
+        public String brf;
+        public String txt;
+    }
+    public class Uv{
+        public String brf;
+        public String txt;
+    }
+
+
+
+}
