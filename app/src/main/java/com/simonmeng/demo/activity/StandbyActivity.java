f6891cf8be3e526a1160cf9478f052b66e8bf9fa
simon9527 <freefanchao@gmail.com>
2016-01-07 11:32:37 +0800

    完成天气预报模块

--- app/src/main/java/com/simonmeng/demo/activity/StandbyActivity.java
+++ app/src/main/java/com/simonmeng/demo/activity/StandbyActivity.java
@@ -2,14 +2,31 @@ package com.simonmeng.demo.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.simonmeng.demo.R;
 
 public class StandbyActivity extends AppCompatActivity {
-
+    private static final String[] strs = new String[] {
+            "first", "second", "third", "fourth", "fifth"
+    };
+    private ListView lv_standby_forecastweather;
+    private View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_standby);
+
+        view = View.inflate(getApplicationContext(),R.layout.item_weatherforcast,null);
+       lv_standby_forecastweather = (ListView)findViewById(R.id.lv_standby_forecastweather);
+        lv_standby_forecastweather.addHeaderView(view);
+       lv_standby_forecastweather.setAdapter(new ArrayAdapter<String>(this,
+               android.R.layout.simple_list_item_1, strs));
     }
+
+
+
 }
+
--- app/src/main/java/com/simonmeng/demo/activity/WeatherActivity.java
+++ app/src/main/java/com/simonmeng/demo/activity/WeatherActivity.java
@@ -6,8 +6,12 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +22,8 @@ import com.google.gson.Gson;
 import com.simonmeng.demo.R;
 import com.simonmeng.demo.domain.WeatherBean;
 
+import java.util.List;
+
 public class WeatherActivity extends AppCompatActivity {
     private String tag = "WeatherActivity";
     private SharedPreferences sp;
@@ -27,12 +33,16 @@ public class WeatherActivity extends AppCompatActivity {
     private int max;
     private int min;
     private int tmp;
+    private WeatherBean bean;
+    private Gson gson;
 
     LinearLayout ll_weather_background;
+    ListView lv_weather_forecastweather;
     TextView tv_weather_location;
     TextView tv_weather_temperature;
     TextView tv_weather_pm;
     TextView tv_weather_maxmin;
+    LinearLayout ll_weather_listheader;
 
 
     @Override
@@ -44,6 +54,7 @@ public class WeatherActivity extends AppCompatActivity {
         tv_weather_pm = (TextView) findViewById(R.id.tv_weather_pm);
         tv_weather_maxmin = (TextView) findViewById(R.id.tv_weather_maxmin);
         ll_weather_background = (LinearLayout) findViewById(R.id.ll_weather_background);
+        lv_weather_forecastweather = (ListView) findViewById(R.id.lv_weather_forecastweather);
 
         //设置字体，typeface字体。
         Typeface face = Typeface.createFromAsset(getAssets(), "deftone stylus.ttf");
@@ -55,6 +66,8 @@ public class WeatherActivity extends AppCompatActivity {
         String location = sp.getString("location","北京");
 
         getDefaultWeather(location);
+
+
 
 
 
@@ -91,10 +104,11 @@ public class WeatherActivity extends AppCompatActivity {
                         //Gson解析服务器返回的json数据---responseString
                         String cityWeather = responseString.replace("HeWeather data service 3.0", "CityWeather");
                         System.out.print(cityWeather);
-                        Gson gson = new Gson();
-                        WeatherBean bean = gson.fromJson(cityWeather, WeatherBean.class);
+                        gson = new Gson();
+                        bean = gson.fromJson(cityWeather, WeatherBean.class);
                         String statue = bean.CityWeather.get(0).status;
-                        if (statue.equalsIgnoreCase("ok")) {
+                        WeatherBean.Aqi aqi = bean.CityWeather.get(0).aqi;
+                        if (statue.equalsIgnoreCase("ok") && aqi!=null) {
                             weather_location = bean.CityWeather.get(0).basic.city;
                             weather_pm = bean.CityWeather.get(0).aqi.city.pm25;
                             weather_qlty = bean.CityWeather.get(0).aqi.city.qlty;
@@ -105,6 +119,12 @@ public class WeatherActivity extends AppCompatActivity {
                             tv_weather_pm.setText("Pm2.5:" + weather_pm + "\n" + weather_qlty);
                             tv_weather_maxmin.setText("Max ↑" + max + "°\n  Mix ↓" + min + "°");
                             tv_weather_temperature.setText(tmp + "°C");
+                            Log.i("sdkdemo", "onSuccess");
+                            System.out.println(bean.CityWeather.get(0).daily_forecast.get(1).tmp.max+"");
+                            WeatherForcastAdapter WeatherForcastAdapter = new WeatherForcastAdapter();
+                            //2.3之后取出顶部底部的阴影，2.3之前xml中android:fadingEdge="none"即可
+                            lv_weather_forecastweather.setOverScrollMode(View.OVER_SCROLL_NEVER);
+                            lv_weather_forecastweather.setAdapter(WeatherForcastAdapter);
                             //获取城市的id，去掉前两位的字母，剩下的都是数字，可以通过switch判断，来设置背景
                             int cityid = Integer.parseInt(bean.CityWeather.get(0).basic.id.substring(2));
                             String location = bean.CityWeather.get(0).basic.city;
@@ -132,8 +152,10 @@ public class WeatherActivity extends AppCompatActivity {
                                     ll_weather_background.setBackgroundResource(R.mipmap.city_default);
                                     break;
                             }
-                        } else if (statue.equalsIgnoreCase("unknown city")) {
+                        } else{//情况1：status是"unknown city"；情况2：三线城市没有aqi
                             Toast.makeText(WeatherActivity.this,"没有查到该城市",Toast.LENGTH_LONG).show();
+                            NoWeaherAdapter noWeaherAdapter = new NoWeaherAdapter();
+                            lv_weather_forecastweather.setAdapter(noWeaherAdapter);
                         }
                     }
 
@@ -152,4 +174,99 @@ public class WeatherActivity extends AppCompatActivity {
     }
 
     //TODO 创建一个listview展示天气预报
+
+    private class WeatherForcastAdapter extends BaseAdapter{
+        List<WeatherBean.Daily_forecast> daily_forecasts = null;
+        @Override
+        public int getCount() {
+            return 8;
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(position==0){
+                TextView tvHeader = new TextView(getApplicationContext());
+                tvHeader.setText("Weather Forecast ");
+                Typeface face = Typeface.createFromAsset(getAssets(), "deftone stylus.ttf");
+                tvHeader.setTypeface(face);
+                tvHeader.setTextColor(getResources().getColor(R.color.colorWhite));
+                tvHeader.setTextSize(25);
+                tvHeader.setBackgroundColor(00000000);
+                tvHeader.setPadding(0, 700, 0, 0);
+                tvHeader.setGravity(Gravity.CENTER);
+                return tvHeader;
+            }else if(position==7){
+                TextView tvHeader = new TextView(getApplicationContext());
+                tvHeader.setText("天气转变 冷暖先知");
+                tvHeader.setTextColor(getResources().getColor(R.color.colorWhite));
+                tvHeader.setTextSize(15);
+                tvHeader.setBackgroundColor(00000000);
+                tvHeader.setPadding(0, 0, 0, 300);
+                tvHeader.setGravity(Gravity.CENTER);
+                return tvHeader;
+            }
+            View view = null;
+            ViewHolder viewHolder = null;
+            if(convertView!=null&&convertView instanceof LinearLayout){
+                view = convertView;
+                viewHolder = (ViewHolder) view.getTag();
+            }else{
+                view = View.inflate(getApplicationContext(),R.layout.item_weatherforcast,null);
+                viewHolder = new ViewHolder();
+                viewHolder.tv_item_weatherforcast_date = (TextView) view.findViewById(R.id.tv_item_weatherforcast_date);
+                viewHolder.tv_item_weatherforcast_min = (TextView) view.findViewById(R.id.tv_item_weatherforcast_min);
+                viewHolder.tv_item_weatherforcast_max = (TextView) view.findViewById(R.id.tv_item_weatherforcast_max);
+                view.setTag(viewHolder);
+            }
+            //设置字体
+            Typeface face = Typeface.createFromAsset(getAssets(), "deftone stylus.ttf");
+            viewHolder.tv_item_weatherforcast_date.setTypeface(face);
+            viewHolder.tv_item_weatherforcast_min.setTypeface(face);
+            viewHolder.tv_item_weatherforcast_max.setTypeface(face);
+            if(bean.CityWeather.get(0)!=null){
+                List<WeatherBean.Daily_forecast> daily_forecasts =  bean.CityWeather.get(0).daily_forecast;
+                viewHolder.tv_item_weatherforcast_date.setText(daily_forecasts.get(position).date);
+                viewHolder.tv_item_weatherforcast_min.setText("Mix ↓"+daily_forecasts.get(position).tmp.min+"°");
+                viewHolder.tv_item_weatherforcast_max.setText("Max ↑"+daily_forecasts.get(position).tmp.max+"°");
+                return view;
+            }else {
+                return null;
+            }
+
+        }
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+    }
+    class ViewHolder{
+        TextView tv_item_weatherforcast_date;
+        TextView tv_item_weatherforcast_min;
+        TextView tv_item_weatherforcast_max;
+    }
+
+    private class NoWeaherAdapter extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
 }
